95d2c4f89103aa26a217fe0bbbd81357e0ba2545
hemaolong <maolong.he@tmail.com>
2013-02-28 20:33:37 +0800

    GoUI

--- src/main/go/main.go
+++ src/main/go/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+  "github.com/lxn/go-winapi"
+  "syscall"
+  "strconv"
+)
+
+func _TEXT(svt string) *uint16{
+	return syscall.StringToUTF16Ptr(svt)
+}
+
+func toString(n int32) string{
+	return strconv.Itoa(int(n));
+}
 
 func main(){
-  fmt.Printf("Hello World")
+	var hwnd winapi.HWND
+	// cxScreen := winapi.GetSystemMetrics(winapi.SM_CXSCREEN)
+	// cyScreen := winapi.GetSystemMetrics(winapi.SM_CYSCREEN)
+	winapi.MessageBox(hwnd, _TEXT("狗语言消息"), _TEXT("ff"), winapi.MB_OK)
 }
