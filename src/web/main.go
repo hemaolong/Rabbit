d41d3a5c011bcc972ea076faf105210acaea4316
hemaolong <maolong.he@tmail.com>
2013-03-29 18:01:54 +0800

    add web

--- src/web/main.go
+++ src/web/main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"net/http"
+	"reflect"
+	"strings"
 )
 
-type page struct {
-	title string
-	body  []byte
-}
+type (
+	page struct {
+		title string
+		body  []byte
+	}
+
+	loginController struct{
+
+	}
+)
 
 func (p *page) save() error {
 	fname := p.title + ".txt"
@@ -32,7 +41,42 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.title, string(p.body))
 	}
 }
+
+///////
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		http.Redirect(w, r, "/login/index", http.StatusFound)
+	}
+
+	if t, err := template.ParseFiles("template/html/404.html"); err != nil {
+		t.Execute(w, nil)
+	}
+}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	pathInfo := strings.Trim(r.URL.Path, "/")
+	parts := strings.Split(pathInfo, "/")
+	action := ""
+	if len(parts) > 1 {
+		action = strings.Title(parts[1]) + "Action"
+	}
+
+	login := &loginController{}
+	controller := reflect.ValueOf(login)
+	method := controller.ValueOf(action)
+	if !method.IsValid() {
+		method = controller.MethodByName(strings.Title("index") + "Action")
+	}
+	requestValue := reflect.ValueOf(r)
+	responseValue = reflect.ValueOf(w)
+	method.Call([]reflect.Value{responseValue, requestValue})
+}
 func main() {
-	http.HandleFunc("/view/", viewHandler)
-	http.ListenAndServe(":8080", nil)
+	http.Handle("/css/", http.FileServer(http.Dir("template")))
+	http.Handle("/js/", httpFileServer(http.Dir("template")))
+
+	http.HandleFunc("/admin/", adminHandler)
+	http.HandleFunc("/login/", loginHandler)
+	http.HandleFunc("/ajax/", ajaxHandler)
+	http.HandleFunc("/", notFoundHandler)
+	http.ListenAndServe(":8888", nil)
 }
